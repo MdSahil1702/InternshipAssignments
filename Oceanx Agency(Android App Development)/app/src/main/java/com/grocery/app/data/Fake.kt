9d837b4db042c3e111d6334752db1260bc52935c
Md Sahil Siddiqui <sahilsiddiqui1702@gmail.com>
2026-05-09 10:53:18 +0530

    create a fake data for the product that will be shown on the home screen without backend

---
+++ Oceanx Agency(Android App Development)/app/src/main/java/com/grocery/app/data/Fake.kt
@@ -0,0 +1,23 @@
+package com.grocery.app.data
+
+import com.grocery.app.data.Product
+
+object Fake{}
+val OTP ="1234"
+
+val products = listOf(
+    Product(1,  "Apple",        40.0,  "https://img.icons8.com/color/96/apple.png",        "Fruits"),
+    Product(2,  "Banana",       20.0,  "https://img.icons8.com/color/96/banana.png",       "Fruits"),
+    Product(3,  "Mango",        60.0,  "https://img.icons8.com/color/96/mango.png",        "Fruits"),
+    Product(4,  "Milk",         55.0,  "https://img.icons8.com/color/96/milk.png",         "Dairy"),
+    Product(5,  "Butter",       45.0,  "https://img.icons8.com/color/96/butter.png",       "Dairy"),
+    Product(6,  "Cheese",       90.0,  "https://img.icons8.com/color/96/cheese.png",       "Dairy"),
+    Product(7,  "Tomato",       30.0,  "https://img.icons8.com/color/96/tomato.png",       "Veggies"),
+    Product(8,  "Potato",       25.0,  "https://img.icons8.com/color/96/potato.png",       "Veggies"),
+    Product(9,  "Onion",        18.0,  "https://img.icons8.com/color/96/onion.png",        "Veggies"),
+    Product(10, "Bread",        35.0,  "https://img.icons8.com/color/96/bread.png",        "Bakery"),
+    Product(11, "Eggs",         70.0,  "https://img.icons8.com/color/96/eggs.png",         "Bakery"),
+    Product(12, "Orange Juice", 80.0,  "https://img.icons8.com/color/96/orange-juice.png", "Drinks")
+)
+
+val cats = listOf("All", "Fruits", "Dairy", "Veggies", "Bakery", "Drinks")
