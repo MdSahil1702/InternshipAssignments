5050bab07ea193be584b225bb0e3b9ba4f63bc93
Md Sahil Siddiqui <sahilsiddiqui1702@gmail.com>
2026-05-11 14:39:33 +0530

    move images to src->main->res->drawable

--- Oceanx Agency(Android App Development)/app/src/main/java/com/grocery/app/utils/Fake.kt
+++ Oceanx Agency(Android App Development)/app/src/main/java/com/grocery/app/utils/Fake.kt
@@ -1,24 +1,25 @@
 package com.grocery.app.utils
 
 import com.grocery.app.data.Product
+import com.grocery.app.R
 
 object Fake {
 
     const val OTP = "1234"
 
     val products = listOf(
-        Product(1,  "Apple",        40.0,  "https://img.icons8.com/color/96/apple.png",        "Fruits"),
-        Product(2,  "Banana",       20.0,  "https://img.icons8.com/color/96/banana.png",       "Fruits"),
-        Product(3,  "Mango",        60.0,  "https://img.icons8.com/color/96/mango.png",        "Fruits"),
-        Product(4,  "Milk",         55.0,  "https://img.icons8.com/color/96/milk.png",         "Dairy"),
-        Product(5,  "Butter",       45.0,  "https://img.icons8.com/color/96/butter.png",       "Dairy"),
-        Product(6,  "Cheese",       90.0,  "https://img.icons8.com/color/96/cheese.png",       "Dairy"),
-        Product(7,  "Tomato",       30.0,  "https://img.icons8.com/color/96/tomato.png",       "Veggies"),
-        Product(8,  "Potato",       25.0,  "https://img.icons8.com/color/96/potato.png",       "Veggies"),
-        Product(9,  "Onion",        18.0,  "https://img.icons8.com/color/96/onion.png",        "Veggies"),
-        Product(10, "Bread",        35.0,  "https://img.icons8.com/color/96/bread.png",        "Bakery"),
-        Product(11, "Eggs",         70.0,  "https://img.icons8.com/color/96/eggs.png",         "Bakery"),
-        Product(12, "Orange Juice", 80.0,  "https://img.icons8.com/color/96/orange-juice.png", "Drinks")
+        Product(1,  "Apple",        40.0,  R.drawable.apples,        "Fruits"),
+        Product(2,  "Banana",       20.0,  R.drawable.bananas,       "Fruits"),
+        Product(3,  "Mango",        60.0,  R.drawable.mangos,        "Fruits"),
+        Product(4,  "Milk",         55.0,  R.drawable.milk,          "Dairy"),
+        Product(5,  "Butter",       45.0,  R.drawable.butter,        "Dairy"),
+        Product(6,  "Cheese",       90.0,  R.drawable.cheese,        "Dairy"),
+        Product(7,  "Tomato",       30.0,  R.drawable.tomatoes,      "Veggies"),
+        Product(8,  "Potato",       25.0,  R.drawable.potatoes,      "Veggies"),
+        Product(9,  "Onion",        18.0,  R.drawable.onion,         "Veggies"),
+        Product(10, "Bread",        35.0,  R.drawable.bread,         "Bakery"),
+        Product(11, "Eggs",         70.0,  R.drawable.eggs,          "Bakery"),
+        Product(12, "Orange Juice", 80.0,  R.drawable.orange_juice,  "Drinks")
     )
 
     val cats = listOf("All", "Fruits", "Dairy", "Veggies", "Bakery", "Drinks")
